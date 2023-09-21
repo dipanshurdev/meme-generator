b8c84a2ff91d35bd47e2f99633888af054c9e77d
Aman <dv451197@gmail.com>
2023-09-21 22:40:15 +0530

    Create Memes

---
+++ src/Components/Meme.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import memesData from "../meme-data.js";
+
+export const Meme = () => {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemes, setAllMemes] = useState([]);
+
+  function getMemeImage() {
+    // const memesArray = allMemes.data.memes;
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
+  const getAPI = async () => {
+    const api = await fetch("https://api.imgflip.com/get_memes");
+    const response = await api.json();
+    setAllMemes(response.data.memes);
+    // console.log(allMemes);
+  };
+  // console.log(allMemes);
+
+  useEffect(() => {
+    getAPI();
+    // console.log("sexy lady");
+  }, []);
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
+};
